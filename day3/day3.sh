0d290db3a318ed79234692df3e1d4f434c6bb733
Matthieu Bollot <m.bollot@code-troopers.com>
2021-12-03 11:57:41 +0100

    feat: 🎸 Add soluce 2 of day 3

--- day3/day3.sh
+++ day3/day3.sh
@@ -17,3 +17,41 @@ do
   fi
 done
 echo $(( 2#$s1 * 2#$s2 ))
+
+cp input.txt input.bak
+nbChar=$(( $(head -n1 input.txt | wc -c| cut -d' ' -f1) - 2 ))
+s1=''
+for i in {0..$nbChar}
+do
+  nb=$(grep "^${s1}1" input.txt | wc -l | cut -d' ' -f1)
+  nbRows=$(wc -l input.txt | cut -d' ' -f1)
+  if [ $(($nb*2)) -ge $nbRows ]; then
+    s1=$(echo $s1'1')
+  else
+    s1=$(echo $s1'0')
+  fi
+  sed -i /^$s1/!d input.txt
+  [ $(wc -l input.txt | cut -d' ' -f1) -le 1 ] && break
+done
+r1=`cat input.txt`
+
+cp input.bak input.txt
+nbChar=$(( $(head -n1 input.txt | wc -c| cut -d' ' -f1) - 2 ))
+s1=''
+for i in {0..$nbChar}
+do
+  nb=$(grep "^${s1}1" input.txt | wc -l | cut -d' ' -f1)
+  nbRows=$(wc -l input.txt | cut -d' ' -f1)
+  if [ $(($nb*2)) -lt $nbRows ]; then
+    s1=$(echo $s1'1')
+  else
+    s1=$(echo $s1'0')
+  fi
+  sed -i /^$s1/!d input.txt
+  [ $(wc -l input.txt | cut -d' ' -f1) -le 1 ] && break
+done
+r2=`cat input.txt`
+
+echo $(( 2#$r1 * 2#$r2 ))
+
+mv input.bak input.txt
